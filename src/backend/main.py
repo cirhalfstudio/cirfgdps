96efb901e3193a610c507adf042a51d144b7fe63
yxzhin <yxzhin@gmail.com>
2026-03-29 20:24:22 +0200

    fix(main): allow patch && settings methods

--- src/backend/main.py
+++ src/backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[config.FRONTEND_URL],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
