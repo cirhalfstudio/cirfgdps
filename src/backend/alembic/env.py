a107f7ffedbbe8070478731ee71dda0d50c0a552
yxzhin <yxzhin@gmail.com>
2026-03-29 21:19:33 +0200

    fix(alembic): correct import in env.py

--- src/backend/alembic/env.py
+++ src/backend/alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from src.backend.app.shared.utils import Base
-from src.backend.app.core.config import get_config
+from src.backend.app.config import get_config
 
 from alembic import context
 
