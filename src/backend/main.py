5de530c2c6323abd601716b3de6cd937434182bd
yxzhin <yxzhin@gmail.com>
2026-03-29 20:22:25 +0200

    fix(main): allow frontend url origin instead of all

--- src/backend/main.py
+++ src/backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[config.FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
