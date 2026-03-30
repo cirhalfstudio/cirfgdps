513e29826e2a26e05c3bdf5cc6883fa64764ba41
yxzhin <yxzhin@gmail.com>
2026-03-30 23:42:51 +0200

    feat(players): add all value objects to init file

---
+++ src/backend/app/services/players/domain/value_objects/__init__.py
@@ -0,0 +1,14 @@
+from .email import Email
+from .player_icons import IconKey, ProfileIcons
+from .player_settings import ProfileSettings, SettingKey, SettingType
+from .username import UserName
+
+__all__ = [
+    "Email",
+    "IconKey",
+    "ProfileIcons",
+    "ProfileSettings",
+    "SettingKey",
+    "SettingType",
+    "UserName",
+]
