23923c02795409f56f80ffb826c6134a1c61bd3d
yxzhin <yxzhin@gmail.com>
2026-03-31 00:48:02 +0200

    refactor(players): rename icons_json to icons && settings_json to settings in dbplayer

--- src/backend/app/services/players/infrastructure/db_models/db_player.py
+++ src/backend/app/services/players/infrastructure/db_models/db_player.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from typing import Any
 
 from sqlalchemy import Boolean, DateTime, Index, String
 from sqlalchemy.dialects.postgresql import JSONB
@@ -7,6 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from .....config import get_const
 from .....shared.utils.db import Base
+from ...domain.value_objects import SettingType
 
 const = get_const()
 
@@ -38,13 +38,15 @@ class DBPlayer(Base):
         nullable=False,
         default=False,
     )
-    icons_json: Mapped[str] = mapped_column(
+    icons: Mapped[dict[str, int]] = mapped_column(
         JSONB,
         nullable=False,
+        name="icons_json",
     )
-    settings_json: Mapped[dict[str, Any]] = mapped_column(
+    settings: Mapped[dict[str, SettingType]] = mapped_column(
         JSONB,
         nullable=False,
+        name="settings_json",
     )
     registered_at: Mapped[datetime] = mapped_column(
         DateTime,
@@ -58,8 +60,8 @@ class DBPlayer(Base):
 
     __table_args__ = (
         Index("idx_players_active", is_active, postgresql_where=is_active.is_(True)),
-        Index("idx_players_icons_json", icons_json, postgresql_using="gin"),
-        Index("idx_players_settings_json", settings_json, postgresql_using="gin"),
+        Index("idx_players_icons_json", icons, postgresql_using="gin"),
+        Index("idx_players_settings_json", settings, postgresql_using="gin"),
         Index("idx_players_registered_at", registered_at),
         Index("idx_players_not_deleted", deleted_at, deleted_at.is_(None)),
     )
